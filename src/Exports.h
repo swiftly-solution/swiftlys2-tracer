0df32b476633015e09c1eba185eb167b1081f911
skuzzis <andreieurt@gmail.com>
2026-05-06 19:45:47 +0300

    update(exports): prevent double dump

--- src/Exports.h
+++ src/Exports.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "StackManager.h"
+#include <atomic>
 
 #ifndef _WIN32
 #define EXPORT_API extern "C" __attribute__((visibility("default")))
@@ -8,7 +9,25 @@
 #define EXPORT_API extern "C" __declspec(dllexport)
 #endif
 
-EXPORT_API void SW2TracerDump(const char* path)
+struct DumpGuard
 {
-  GlobalStackManager()->Dump(path);
+    std::atomic_flag &flag;
+    ~DumpGuard()
+    {
+        flag.clear(std::memory_order_release);
+    }
+};
+
+EXPORT_API void SW2TracerDump(const char *path)
+{
+    if (path == nullptr || path[0] == '\0')
+        return;
+
+    static std::atomic_flag dumpInProgress = ATOMIC_FLAG_INIT;
+    if (dumpInProgress.test_and_set(std::memory_order_acquire))
+        return;
+
+    DumpGuard guard{dumpInProgress};
+
+    GlobalStackManager()->Dump(path);
 }
